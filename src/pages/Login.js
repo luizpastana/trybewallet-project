2f87b75f76f05f078031b63482f2e96b50b88636
luizpastana <lcsp84@gmail.com>
2022-05-18 18:48:17 -0300

    feat: inclui as funcoes que controlam o inpute suas validacoes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,35 +4,58 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nameInput: '',
+      emailInput: '',
+      senhaInput: '',
+      buttonDisable: true,
     };
   }
 
+  hendleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value }, () => this.emailValidation());
+  }
+
+  emailValidation = () => {
+    const { emailInput, senhaInput } = this.state;
+    const FIVE = 5;
+    const validEmail = emailInput.includes('@');
+    const validEmail2 = emailInput.includes('.com');
+    const validSenha = senhaInput.length > FIVE;
+    if (validSenha && validEmail && validEmail2) {
+      this.setState({ buttonDisable: false });
+    } else {
+      this.setState({ buttonDisable: true });
+    }
+  }
+
   render() {
-    const { nameInput } = this.state;
+    const { emailInput, senhaInput, buttonDisable } = this.state;
     return (
       <section>
-        <label htmlFor="nameInput">
+        <label htmlFor="emailInput">
           Email:
           <input
             data-testid="email-input"
-            name="nameInput"
-            id="nameInput"
+            name="emailInput"
+            id="emailInput"
             type="text"
-            value={ nameInput }
+            value={ emailInput }
+            onChange={ this.hendleChange }
           />
         </label>
-        <label htmlFor="nameInput">
+        <label htmlFor="senhaInput">
           Senha:
           <input
             data-testid="password-input"
             name="senhaInput"
             id="senhaInput"
-            type="text"
+            type="password"
+            value={ senhaInput }
+            onChange={ this.hendleChange }
           />
         </label>
         <button
           type="button"
+          disabled={ buttonDisable }
         >
           Entrar
         </button>
