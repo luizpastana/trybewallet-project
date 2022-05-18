f018429d6e1c584c2430bd5d40fdcef1e67fe263
luizpastana <lcsp84@gmail.com>
2022-05-18 17:54:11 -0300

    feat: Cria a page login e seus inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameInput: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { nameInput } = this.state;
+    return (
+      <section>
+        <label htmlFor="nameInput">
+          Email:
+          <input
+            data-testid="email-input"
+            name="nameInput"
+            id="nameInput"
+            type="text"
+            value={ nameInput }
+          />
+        </label>
+        <label htmlFor="nameInput">
+          Senha:
+          <input
+            data-testid="password-input"
+            name="senhaInput"
+            id="senhaInput"
+            type="text"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </section>
+    );
   }
 }
 
