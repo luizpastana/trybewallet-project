664e5f3ccaecbacec6ee35014cc7fbc3327abedc
luizpastana <lcsp84@gmail.com>
2022-05-19 10:13:39 -0300

    feat: Altera o componente Wallet para exibir o email do usuario e conclui o requisito 03

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    return (
+      <>
+        <header>
+          <span
+            data-testid="email-field"
+          >
+            {userEmail}
+          </span>
+          <p
+            data-testid="total-field"
+          >
+            {0}
+          </p>
+          <p
+            data-testid="header-currency-field"
+          >
+            BRL
+          </p>
+        </header>
+        <p>Esseé prolint</p>
+      </>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
