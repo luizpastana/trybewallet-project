65d84ef44b138485e6a9ca3f26abaf8d56d6afe1
luizpastana <lcsp84@gmail.com>
2022-05-25 08:35:01 -0300

    feat: Cria o componente Table com o cabeçalho requerido e conclui o requisito 07

---
+++ src/components/Table.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+
+class Table extends React.Component {
+  render() {
+    return (
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </table>
+    );
+  }
+}
+
+export default connect()(Table);
