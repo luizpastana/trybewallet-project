8c1703e961c13b0fd5ee4db91588ec1719537029
luizpastana <lcsp84@gmail.com>
2022-11-10 20:40:34 -0300

    css: implementa estilização do login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { connect } from 'react-redux';
 import { actionLogin } from '../actions';
+import walletImg from '../images/wallet.jpg';
 
 class Login extends React.Component {
   constructor(props) {
@@ -39,45 +40,63 @@ class Login extends React.Component {
     this.setState({ emailInput: '', senhaInput: '' });
     history.push('/carteira');
   }
+  // className="flex-column-reverse align-content-center"
 
   render() {
     const { emailInput, senhaInput, buttonDisable } = this.state;
     return (
-      <section className="container d-flex flex-column pt-4 bg-dark text-white">
-        <div className="mx-auto">
-          <label htmlFor="emailInput">
-            Email :
-            <input
-              data-testid="email-input"
-              name="emailInput"
-              id="emailInput"
-              type="text"
-              value={ emailInput }
-              onChange={ this.hendleChange }
-            />
-          </label>
+      <section
+        className="
+        d-flex
+        flex-row
+        align-items-center
+        align-content-center
+        p-3
+        bg-dark
+        text-white"
+      >
+        <div className="container">
+          <img alt="wallet" src={ walletImg } className="img-thumbnail" />
         </div>
-        <div className="flex-wrap mx-auto">
-          <label htmlFor="senhaInput">
-            Senha:
-            <input
-              data-testid="password-input"
-              name="senhaInput"
-              id="senhaInput"
-              type="password"
-              value={ senhaInput }
-              onChange={ this.hendleChange }
-            />
-          </label>
+        <div className="d-flex flex-column">
+          <h1 className="text-center text-white font-weight-bold">TryWallet</h1>
+          <div className="text-rigth text-white font-weight-bold">
+            <label htmlFor="emailInput">
+              Email :
+              <input
+                data-testid="email-input"
+                name="emailInput"
+                id="emailInput"
+                type="text"
+                value={ emailInput }
+                onChange={ this.hendleChange }
+              />
+            </label>
+          </div>
+          <div className="text-rigth text-white font-weight-bold">
+            <label htmlFor="senhaInput">
+              Senha:
+              <input
+                data-testid="password-input"
+                name="senhaInput"
+                id="senhaInput"
+                type="password"
+                value={ senhaInput }
+                onChange={ this.hendleChange }
+              />
+            </label>
+          </div>
+          <div className="text-center text-white">
+            <button
+              className="btn btn-secondary"
+              type="button"
+              disabled={ buttonDisable }
+              onClick={ this.hendleSubmit }
+            >
+              Entrar
+            </button>
+          </div>
         </div>
-        <button
-          className="btn btn-secondary mx-auto"
-          type="button"
-          disabled={ buttonDisable }
-          onClick={ this.hendleSubmit }
-        >
-          Entrar
-        </button>
       </section>
     );
   }
